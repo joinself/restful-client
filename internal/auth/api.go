da315d676e2b45bc1f8f97d0e5e51bff055c4128
Adrià Cidre <593270+adriacidre@users.noreply.github.com>
2023-03-13 18:37:25 +0100

    docs

--- internal/auth/api.go
+++ internal/auth/api.go
@@ -7,18 +7,32 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type AuthRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type AuthResponse struct {
+	Token string `json:"token"`
+}
+
 // RegisterHandlers registers handlers for different HTTP requests.
 func RegisterHandlers(rg *echo.Group, service Service, logger log.Logger) {
 	rg.POST("/login", login(service, logger))
 }
 
-// login returns a handler that handles user login request.
+// Login 	    godoc
+// @Summary     Authenticate.
+// @Description Get a temporary JWT token to interact with the api.
+// @Tags        login
+// @Accept      json
+// @Produce     json
+// @Param       request   body      AuthRequest  true  "Self ID"
+// @Success     200  {object}  AuthResponse
+// @Router      /login [post]
 func login(service Service, logger log.Logger) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		var req struct {
-			Username string `json:"username"`
-			Password string `json:"password"`
-		}
+		var req AuthRequest
 
 		if err := c.Bind(&req); err != nil {
 			logger.With(c.Request().Context()).Errorf("invalid request: %v", err)
@@ -30,8 +44,6 @@ func login(service Service, logger log.Logger) echo.HandlerFunc {
 			return c.JSON(http.StatusUnauthorized, err.Error())
 		}
 
-		return c.JSON(http.StatusOK, struct {
-			Token string `json:"token"`
-		}{token})
+		return c.JSON(http.StatusOK, AuthResponse{token})
 	}
 }
