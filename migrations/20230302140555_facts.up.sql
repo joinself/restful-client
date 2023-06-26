bebe2f2bd4c3af5acbe925de0091da4a871d318d
Adrià Cidre <593270+adriacidre@users.noreply.github.com>
2023-06-26 15:30:27 +0200

    implement requests

--- migrations/20230302140555_facts.up.sql
+++ migrations/20230302140555_facts.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE fact
 (
     id              VARCHAR NOT NULL UNIQUE DEFAULT gen_random_uuid(),
     connection_id   INTEGER NOT NULL,
+    request_id      VARCHAR(255) NOT NULL,
     iss             VARCHAR(255) DEFAULT '' NOT NULL,
     cid             VARCHAR(255) DEFAULT '' NOT NULL,
     jti             VARCHAR(255) DEFAULT '' NOT NULL,
@@ -14,5 +15,8 @@ CREATE TABLE fact
     updated_at      TIMESTAMP NOT NULL,
     CONSTRAINT fk_connection
       FOREIGN KEY(connection_id) 
-	  REFERENCES connection(id)
+	  REFERENCES connection(id),
+    CONSTRAINT fk_request
+      FOREIGN KEY(request_id)
+	  REFERENCES request(id)
 );
