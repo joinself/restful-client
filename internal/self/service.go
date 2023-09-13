1c123f3d5b3f80dce40537c266fbe3356b9c920b
Adrià Cidre <593270+adriacidre@users.noreply.github.com>
2023-09-13 17:41:38 +0200

    Fix dpanic log

--- internal/self/service.go
+++ internal/self/service.go
@@ -83,10 +83,10 @@ func NewService(c Config) Service {
 
 // Run executes the background self listerners.
 func (s *service) Run() {
-	s.logger.With(context.Background(), "self").Info("starting self client")
+	s.logger.With(context.Background()).Info("starting self client")
 	err := s.client.Start()
 	if err != nil {
-		s.logger.With(context.Background(), "self").Error(err.Error())
+		s.logger.With(context.Background()).Error(err.Error())
 	}
 }
 
