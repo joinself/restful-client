869c1c14bd81e3f81ae18d243a9d94a075b5e2dd
Adrià Cidre <593270+adriacidre@users.noreply.github.com>
2024-02-27 14:26:56 +0100

    Breakdown authentication endpoints
    Breakdown authentication /login endpoint into /login and /refresh, so
    the second one manages the token refresh instead of being both managed
    through the /login endpoint.

--- internal/auth/api.go
+++ internal/auth/api.go
@@ -11,6 +11,7 @@ import (
 // RegisterHandlers registers handlers for different HTTP requests.
 func RegisterHandlers(rg *echo.Group, service Service, logger log.Logger) {
 	rg.POST("/login", login(service, logger))
+	rg.POST("/refresh", refresh(service, logger))
 }
 
 // Login godoc
@@ -19,7 +20,7 @@ func RegisterHandlers(rg *echo.Group, service Service, logger log.Logger) {
 // @Tags Authentication
 // @Accept json
 // @Produce json
-// @Param request body LoginRequest true "Authentication request body with your username and password, or a refresh token"
+// @Param request body LoginRequest true "Authentication request body with your username and password"
 // @Success 200 {object} LoginResponse "Successfully authenticated, JWT token and Refresh JWT token are returned in response"
 // @Failure 401,400 {object} response.Error "Returns error details"
 // @Router /login [post]
@@ -40,24 +41,53 @@ func login(service Service, logger log.Logger) echo.HandlerFunc {
 		var resp LoginResponse
 		var ctx = c.Request().Context()
 
-		if req.RefreshToken != "" { // Is a refresh based auth workflow.
-			resp, err = service.Refresh(ctx, req.RefreshToken)
-			if err != nil {
-				return c.JSON(http.StatusUnauthorized, response.Error{
-					Status:  http.StatusUnauthorized,
-					Error:   "You're unauthorized to perform this action",
-					Details: "You've provided a refresh_token, but it's not valid",
-				})
-			}
-		} else { // Is a basic auth workflow.
-			resp, err = service.Login(ctx, req.Username, req.Password)
-			if err != nil {
-				return c.JSON(http.StatusUnauthorized, response.Error{
-					Status:  http.StatusUnauthorized,
-					Error:   "You're unauthorized to perform this action",
-					Details: "Provided auth credentials are invalid",
-				})
-			}
+		resp, err = service.Login(ctx, req.Username, req.Password)
+		if err != nil {
+			return c.JSON(http.StatusUnauthorized, response.Error{
+				Status:  http.StatusUnauthorized,
+				Error:   "You're unauthorized to perform this action",
+				Details: "Provided auth credentials are invalid",
+			})
+		}
+
+		return c.JSON(http.StatusOK, resp)
+	}
+}
+
+// Refresh godoc
+// @Summary Refresh JWT token
+// @Description Takes a refresh token and returns a new JWT token for API interaction.
+// @Tags Authentication
+// @Accept json
+// @Produce json
+// @Param request body RefreshRequest true "Request body with your refresh token"
+// @Success 200 {object} LoginResponse "Successfully refreshed, new JWT token is returned in response"
+// @Failure 401,400 {object} response.Error "Returns error details"
+// @Router /refresh [post]
+func refresh(service Service, logger log.Logger) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var req RefreshRequest
+
+		if err := c.Bind(&req); err != nil {
+			logger.With(c.Request().Context()).Errorf("invalid request: %v", err)
+			return c.JSON(response.DefaultBadRequestError())
+		}
+
+		if reqErr := req.Validate(); reqErr != nil {
+			return c.JSON(reqErr.Status, reqErr)
+		}
+
+		var err error
+		var resp LoginResponse
+		var ctx = c.Request().Context()
+
+		resp, err = service.Refresh(ctx, req.RefreshToken)
+		if err != nil {
+			return c.JSON(http.StatusUnauthorized, response.Error{
+				Status:  http.StatusUnauthorized,
+				Error:   "You're unauthorized to perform this action",
+				Details: "You've provided a refresh_token, but it's not valid",
+			})
 		}
 
 		return c.JSON(http.StatusOK, resp)
--- internal/auth/api_test.go
+++ internal/auth/api_test.go
@@ -73,19 +73,50 @@ func TestLoginAPIEndpoint(t *testing.T) {
 			WantStatus:   http.StatusBadRequest,
 			WantResponse: "",
 		},
+	}
+	for _, tc := range tests {
+		test.Endpoint(t, router, tc)
+	}
+}
+
+func TestRefreshAPIEndpoint(t *testing.T) {
+	logger, _ := log.NewForTest()
+	router := test.MockRouter(logger)
+
+	RegisterHandlers(router.Group(""), mockService{}, logger)
+
+	tests := []test.APITestCase{
 		{
-			Name:         "refresh token",
+			Name:         "success",
 			Method:       "POST",
-			URL:          "/login",
+			URL:          "/refresh",
 			Body:         `{"refresh_token":"test_token_test_token_test_token_test_token_test_token_"}`,
 			Header:       nil,
 			WantStatus:   http.StatusOK,
 			WantResponse: `{"token":"token-100","refresh_token":"r-token-100"}`,
 		},
 		{
+			Name:         "bad json",
+			Method:       "POST",
+			URL:          "/refresh",
+			Body:         `{]`,
+			Header:       nil,
+			WantStatus:   http.StatusBadRequest,
+			WantResponse: "",
+		},
+		{
+			Name:         "invalid data",
+			Method:       "POST",
+			URL:          "/refresh",
+			Body:         `{"refresh_token":""}`,
+			Header:       nil,
+			WantStatus:   http.StatusBadRequest,
+			WantResponse: "",
+		},
+		{
 			Name:         "refresh token errored",
 			Method:       "POST",
-			URL:          "/login",
+			URL:          "/refresh",
 			Body:         `{"refresh_token":"test_token_test_token_test_token_test_token_test_token_error"}`,
 			Header:       nil,
 			WantStatus:   http.StatusUnauthorized,
